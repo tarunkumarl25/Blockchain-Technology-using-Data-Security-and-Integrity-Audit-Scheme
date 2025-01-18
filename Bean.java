33fd5c81f36effec42e3714907e9acfd9e907833
Tarun Kumar <tarunkumarl25604@gmail.com>
2025-01-18 20:46:36 +0530

    Java File

---
+++ Bean.java
@@ -0,0 +1,135 @@
+package deduplicatable.data.auditing.mechanismbean;
+
+public class Bean {
+	private int uid;
+	private String name;
+	private String email;
+	private String password;
+	private String dob;
+	private String mobile;
+	private int tpaid;
+	private String tpaName;
+	private String address;
+	
+	private String fname;
+	private String data;
+	private int hashcode;
+	private String hashvalue;
+	private String block;
+	private int blocksize;
+	private String pkey;
+	private String skey;
+	private int fid;
+	
+	
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public int getTpaid() {
+		return tpaid;
+	}
+	public void setTpaid(int tpaid) {
+		this.tpaid = tpaid;
+	}
+	public String getTpaName() {
+		return tpaName;
+	}
+	public void setTpaName(String tpaName) {
+		this.tpaName = tpaName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public int getHashcode() {
+		return hashcode;
+	}
+	public void setHashcode(int hashcode) {
+		this.hashcode = hashcode;
+	}
+	public String getHashvalue() {
+		return hashvalue;
+	}
+	public void setHashvalue(String hashvalue) {
+		this.hashvalue = hashvalue;
+	}
+	public String getBlock() {
+		return block;
+	}
+	public void setBlock(String block) {
+		this.block = block;
+	}
+	public int getBlocksize() {
+		return blocksize;
+	}
+	public void setBlocksize(int blocksize) {
+		this.blocksize = blocksize;
+	}
+	public String getPkey() {
+		return pkey;
+	}
+	public void setPkey(String pkey) {
+		this.pkey = pkey;
+	}
+	public String getSkey() {
+		return skey;
+	}
+	public void setSkey(String skey) {
+		this.skey = skey;
+	}
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	
+	
+}
